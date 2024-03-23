91da5b8e9b4eb96cb0f511882335ab1eab225868
AkashFestivs <gadhaveakash999@gmail.com>
2024-03-23 13:16:23 +0530

    commit

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,6 +1,7 @@
 import { PageBreadCrumb } from '@/components/layout'
 import React from 'react'
 import Image from "next/image";
+import Link from "next/link";
 
 const ServicePage = () => {
   return (
@@ -162,9 +163,12 @@ const ServicePage = () => {
           </p>
 
           <div className="inline-flex w-full mt-6 sm:w-auto">
-            <a href="#" className="inline-flex items-center justify-center w-full px-6 py-2 text-white duration-300 bg-blue-600 rounded-lg hover:bg-blue-500 focus:ring focus:ring-blue-300 focus:ring-opacity-80">
-              Sign Up
-            </a>
+          <div className="pt-8">
+            <Link href="/contact" className="animated-button">
+              Contact Us
+              <i aria-hidden className="fa-solid fa-arrow-right"></i>
+            </Link>
+          </div>
           </div>
         </div>
       </section>
