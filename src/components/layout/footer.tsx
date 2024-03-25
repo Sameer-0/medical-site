edecbfc29eec9e75126bf5ec00f44189987ebde8
Sameer-0 <sameershaikhx123@gmail.com>
2024-03-26 00:11:24 +0530

    footer changes

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -31,22 +31,27 @@ const Footer = () => {
             </div>
           </div>
         </div> */}
-        <Separator className="bg-slate-200 h-[2px]" />
-        <div className="px-2 sm:px-6 md:px-16 grid grid-cols-3 md:grid-cols-4 py-8 gap-y-4">
-          <div className="col-span-1 bg-white w-fit p-2 rounded-lg">
+        {/* <Separator className="bg-slate-200 h-[2px]" /> */}
+        <div className="px-2 sm:px-6 md:px-16 flex justify-center items-center py-8 gap-y-4">
+          {/* <div className="col-span-1 bg-white w-fit p-2 rounded-lg">
 
-          </div>
-          <div className="pl-2 text-center col-span-2 md:col-span-2 text-sm md:text-base">
-          <Image
+          </div> */}
+          <div className="pl-2 text-center text-sm md:text-base">
+            <Image
               src="/images/logonbg.webp"
               alt="Logo"
               width={120}
               height={120}
-              className="items-center"
+              className="items-center mx-auto mb-4"
             />
-            <p className="font-serif">Our Mission is to provide individual focused pharmacy</p>
-            <p className="font-serif">Services, Support and advocacy for out patients and patners</p><br />
-            © 2024-2025 Galveston Specialty Pharmacy <br /> All Rights Reserved
+            <p className="font-serif">
+              Our Mission is to provide individual focused pharmacy
+            </p>
+            <p className="font-serif">
+              Services, Support and advocacy for out patients and patners
+            </p>
+            <br />
+            © 2024-2025 Galveston Specialty Pharmacy. All Rights Reserved
           </div>
           <div className="col-span-full md:col-span-1 flex justify-center md:justify-end gap-x-2">
             {/* <Link href="#">
